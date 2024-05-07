07248445d8240086a5961bc07c968b7b1b991002
ValleAlexis <168995055+ValleAlexis@users.noreply.github.com>
2024-05-07 13:06:18 -0500

    Add files via upload

---
+++ bucle/bucle/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //drclaracion de variable bf de tipo buble for
+        int a;
+        BubleFor bf;
+        
+        //instancia mew
+        bf = new BubleFor();
+
+        //llamar a un metodo
+        bf.SignoAlterno();
+    }
+}
---
+++ bucle/bucle/src/BubleFor.java
@@ -0,0 +1,11 @@
+public class BubleFor {
+
+    public void SignoAlterno(){
+        for(int i=1;i<15;i++)
+            if(i%2==0)
+                System.out.print("- ");
+            else
+                System.out.print("+ ");
+        System.out.println("");
+    }
+}
